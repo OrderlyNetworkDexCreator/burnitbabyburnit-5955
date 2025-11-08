01bb12cdde9d888aec562fc7308b160e95f26f9a
Mario Reder <mario.reder@pm.me>
2025-11-08 13:17:41 +0100

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -18,7 +18,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_HAS_PRIMARY_LOGO": "true",
   "VITE_HAS_SECONDARY_LOGO": "true",
   "VITE_ENABLED_MENUS": "Trading,Swap,Portfolio,Markets,Leaderboard,Rewards,Vaults",
-  "VITE_CUSTOM_MENUS": "burnbabyburn, https://burnitbabyburnit.com/",
+  "VITE_CUSTOM_MENUS": "burnbabyburn, https://burnitbabyburnit.com/; Official X page., https://x.com/BBBxOfficial",
   "VITE_ENABLE_CAMPAIGNS": "false",
   "VITE_TELEGRAM_URL": "https://t.me/BurnItBabyBurnIt",
   "VITE_DISCORD_URL": "https://discord.gg/mHStNRkuYk",
